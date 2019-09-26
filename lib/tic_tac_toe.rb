cd16f482a9438e35ab7f7cd84001b10456bb7bb3
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-26 03:33:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,3 +49,6 @@ def turn_count(board)
   token_array = board.any? {|token| token == "X" || token == "O"}
   counter = token_array.length
 end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"

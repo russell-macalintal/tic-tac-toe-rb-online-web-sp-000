bf3b0cf41fb8ac1a839557d997c12437cb73427b
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-26 03:23:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,12 @@ def valid_move?(board, index)
 end
 
 def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, token)
+  else
+    turn(board)
+  end
 end
